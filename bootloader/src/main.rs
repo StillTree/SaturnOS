b3b4274a9c86301ff040f308538db6649b1f696d
Alexander Dębowski <alex.jan.paw.2005@gmail.com>
2024-08-04 23:20:17 +0200

    Initial shitty bootloader that does nothing

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use uefi::{entry, table::{Boot, SystemTable}, Handle, Status};
+use core::fmt::Write;
+
+#[entry]
+fn efi_main(_image: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi::helpers::init(&mut system_table).unwrap();
+
+    let stdout = system_table.stdout();
+    stdout.clear().unwrap();
+    writeln!(stdout, "Welcome to the NutCracker bootloader!").unwrap();
+    writeln!(stdout, "Initializing...").unwrap();
+
+    system_table.boot_services().stall(60_000_000);
+
+    Status::SUCCESS
 }
