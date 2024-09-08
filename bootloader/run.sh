232f8a2312f7022306cb50d232d17a5b9980e48b
Alexander Dębowski <alex.jan.paw.2005@gmail.com>
2024-09-08 20:16:07 +0200

    A slightly better run.sh

--- bootloader/run.sh
+++ bootloader/run.sh
@@ -6,10 +6,23 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
+ENABLE_KVM="-enable-kvm"
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+	ENABLE_KVM=""
+fi
+
+if [ -z "${OVMF_CODE_LOCATION+x}" ]; then
+	OVMF_CODE_LOCATION="/opt/OVMF/OVMF_CODE.fd"
+fi
+
+if [ -z "${OVMF_VARS_LOCATION+x}" ]; then
+	OVMF_VARS_LOCATION="/opt/OVMF/OVMF_VARS.fd"
+fi
+
 qemu-system-x86_64 \
-	-enable-kvm \
+	$ENABLE_KVM \
 	-serial stdio \
-	-drive if=pflash,format=raw,readonly=on,file=/opt/OVMF/OVMF_CODE.fd \
-	-drive if=pflash,format=raw,readonly=on,file=/opt/OVMF/OVMF_VARS.fd \
+	-drive if=pflash,format=raw,readonly=on,file=$OVMF_CODE_LOCATION \
+	-drive if=pflash,format=raw,readonly=on,file=$OVMF_VARS_LOCATION \
 	-drive format=raw,file=fat:rw:esp
 
