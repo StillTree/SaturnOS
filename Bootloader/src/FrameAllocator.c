3f030e54243581373dc31ac82307ebb5a7b8ac05
Alexander Dębowski <alex.jan.paw.2005@gmail.com>
2024-09-21 23:47:26 +0200

    Added an ability to allocate contiguous physical memory regions to the bootloader

--- Bootloader/src/FrameAllocator.c
+++ Bootloader/src/FrameAllocator.c
@@ -30,6 +30,34 @@ EFI_STATUS NextDescriptorFrame(
 	return EFI_OUT_OF_RESOURCES;
 }
 
+/// Returns the first free descriptor in an array with the given size from the given descriptor starting with `lastFrame` (exclusive).
+EFI_STATUS NextDescriptorContiguousFrames(
+	EFI_MEMORY_DESCRIPTOR* memoryDescriptor,
+	EFI_PHYSICAL_ADDRESS lastFrame,
+	UINTN num,
+	EFI_PHYSICAL_ADDRESS* allocatedFramesAddress)
+{
+	const EFI_PHYSICAL_ADDRESS minFrame = PhysFrameContainingAddress(memoryDescriptor->PhysicalStart);
+	const EFI_PHYSICAL_ADDRESS maxFrame = PhysFrameContainingAddress(memoryDescriptor->PhysicalStart + memoryDescriptor->NumberOfPages * 4096 - 1);
+
+	// If the last allocated frame is outside the bounds of the memory descriptor,
+	// we know that the first frame will be available so we return it.
+	if(lastFrame < minFrame && memoryDescriptor->NumberOfPages >= num)
+	{
+		*allocatedFramesAddress = minFrame;
+		return EFI_SUCCESS;
+	}
+
+	// Check if there are any more frames to allocate, and if there are we allocate them.
+	if(lastFrame + (num - 1) * 4096 < maxFrame)
+	{
+		*allocatedFramesAddress = lastFrame + 4096;
+		return EFI_SUCCESS;
+	}
+
+	return EFI_OUT_OF_RESOURCES;
+}
+
 EFI_STATUS InitFrameAllocator(FrameAllocatorData* frameAllocator, EFI_MEMORY_DESCRIPTOR* memoryMap, UINTN memoryMapSize, UINTN descriptorSize)
 {
 	frameAllocator->currentMemoryDescriptor      = memoryMap;
@@ -67,7 +95,7 @@ EFI_STATUS InitFrameAllocator(FrameAllocatorData* frameAllocator, EFI_MEMORY_DES
 
 /// A helper function that allocates the next free frame from the current descriptor,
 /// only if one's available, otherwise returns an error.
-EFI_STATUS AllocateNextCurrentDescriptorFrame(FrameAllocatorData* frameAllocator, EFI_PHYSICAL_ADDRESS* allocatedFrameAddress)
+EFI_STATUS AllocateCurrentDescriptorFrame(FrameAllocatorData* frameAllocator, EFI_PHYSICAL_ADDRESS* allocatedFrameAddress)
 {
 	EFI_STATUS status = NextDescriptorFrame(
 		frameAllocator->currentMemoryDescriptor,
@@ -84,10 +112,30 @@ EFI_STATUS AllocateNextCurrentDescriptorFrame(FrameAllocatorData* frameAllocator
 	return status;
 }
 
+/// A helper function that allocates the next free frames from the current descriptor,
+/// only if a contiguous array is available, otherwise returns an error.
+EFI_STATUS AllocateCurrentDescriptorContiguousFrames(FrameAllocatorData* frameAllocator, UINTN num, EFI_PHYSICAL_ADDRESS* allocatedFrameAddress)
+{
+	EFI_STATUS status = NextDescriptorContiguousFrames(
+		frameAllocator->currentMemoryDescriptor,
+		frameAllocator->previousFrame,
+		num,
+		allocatedFrameAddress);
+	if(EFI_ERROR(status))
+	{
+		return status;
+	}
+
+	// Because the available frames are being consumed we deem the last one of them as the previous, used one.
+	frameAllocator->previousFrame = (*allocatedFrameAddress) + (num - 1) * 4096;
+
+	return status;
+}
+
 EFI_STATUS AllocateFrame(FrameAllocatorData* frameAllocator, EFI_PHYSICAL_ADDRESS* allocatedFrameAddress)
 {
 	// If there is an available frame within the current descriptor's bounds, return it.
-	EFI_STATUS status = AllocateNextCurrentDescriptorFrame(frameAllocator, allocatedFrameAddress);
+	EFI_STATUS status = AllocateCurrentDescriptorFrame(frameAllocator, allocatedFrameAddress);
 	if(!EFI_ERROR(status))
 	{
 		return EFI_SUCCESS;
@@ -115,6 +163,45 @@ EFI_STATUS AllocateFrame(FrameAllocatorData* frameAllocator, EFI_PHYSICAL_ADDRES
 			frameAllocator->currentMemoryDescriptor      = descriptor;
 			frameAllocator->currentMemoryDescriptorIndex += i;
 			frameAllocator->previousFrame                = *allocatedFrameAddress;
+			return EFI_SUCCESS;
+		}
+	}
+
+	return EFI_OUT_OF_RESOURCES;
+}
+
+EFI_STATUS AllocateContiguousFrames(FrameAllocatorData* frameAllocator, UINTN num, EFI_PHYSICAL_ADDRESS* allocatedFrameAddress)
+{
+	// If there are available frames within the current descriptor's bounds, return them.
+	EFI_STATUS status = AllocateCurrentDescriptorContiguousFrames(frameAllocator, num, allocatedFrameAddress);
+	if(!EFI_ERROR(status))
+	{
+		return EFI_SUCCESS;
+	}
+
+	// And if not, we need to find the next available memory descriptor.
+	// If its "usable", allocate the frame array from it.
+	for(UINTN i = 1; i < (frameAllocator->memoryMapSize / frameAllocator->descriptorSize) - frameAllocator->currentMemoryDescriptorIndex; i++)
+	{
+		// sizeof(EFI_MEMORY_DESCRIPTOR) is not the same as its size in memory
+		EFI_MEMORY_DESCRIPTOR* descriptor =
+			(EFI_MEMORY_DESCRIPTOR*) ((UINT8*) frameAllocator->currentMemoryDescriptor + (i * frameAllocator->descriptorSize));
+
+		if(descriptor->Type != EfiConventionalMemory)
+		{
+			continue;
+		}
+
+		status = NextDescriptorContiguousFrames(
+			descriptor,
+			frameAllocator->previousFrame,
+			num,
+			allocatedFrameAddress);
+		if(!EFI_ERROR(status))
+		{
+			frameAllocator->currentMemoryDescriptor      = descriptor;
+			frameAllocator->currentMemoryDescriptorIndex += i;
+			frameAllocator->previousFrame                = (*allocatedFrameAddress) + (num - 1) * 4096;
 			return EFI_SUCCESS;
 		}
 	}
